9a688344cc8bfec85738ec628df1de1716687ebd
Lucas Gérard <lucas.gerard@tiime.fr>
2023-02-15 15:40:42 +0100

    Update Payee types (#17)

--- src/BusinessTermsGroup/Payee.php
+++ src/BusinessTermsGroup/Payee.php
@@ -2,6 +2,9 @@
 
 namespace Tiime\FacturX\BusinessTermsGroup;
 
+use Tiime\FacturX\DataType\Identifier\LegalRegistrationIdentifier;
+use Tiime\FacturX\DataType\Identifier\PayeeIdentifier;
+
 /**
  * BG-10
  * A group of business terms providing information about the Payee, i.e. the role that receives the payment.
@@ -11,11 +14,54 @@ class Payee
     /**
      * BT-59
      * The name of the Payee.
-     *
-     * @var
      */
-    private $name;
+    private string $name;
 
-    /** @todo BT-60 */
-    /** @todo BT-61 */
+    /**
+     * BT-60
+     * An identifier for the Payee.
+     */
+    private ?PayeeIdentifier $identifier;
+
+    /**
+     * BT-61
+     * An identifier issued by an official registrar that identifies the Payee as a legal entity or person.
+     */
+    private ?LegalRegistrationIdentifier $legalRegistrationIdentifier;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+        $this->identifier = null;
+        $this->legalRegistrationIdentifier = null;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getIdentifier(): ?PayeeIdentifier
+    {
+        return $this->identifier;
+    }
+
+    public function setIdentifier(?PayeeIdentifier $identifier): self
+    {
+        $this->identifier = $identifier;
+
+        return $this;
+    }
+
+    public function getLegalRegistrationIdentifier(): ?LegalRegistrationIdentifier
+    {
+        return $this->legalRegistrationIdentifier;
+    }
+
+    public function setLegalRegistrationIdentifier(?LegalRegistrationIdentifier $legalRegistrationIdentifier): self
+    {
+        $this->legalRegistrationIdentifier = $legalRegistrationIdentifier;
+
+        return $this;
+    }
 }
