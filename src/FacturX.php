bfc73f456bd4885231ecb03c168af428e6bb4636
Aurélien Pillevesse <aurelienpillevesse@hotmail.fr>
2023-05-04 13:43:23 +0200

    add wrapper

--- src/FacturX.php
+++ src/FacturX.php
@@ -6,11 +6,66 @@ namespace Tiime\FacturX;
 
 class FacturX
 {
-//    private const VERSION = '1.7';
-//    private const ENCODING = 'UTF-8';
+    //    private const PDF_VERSION = '1.7';
+    //    private const ENCODING = 'UTF-8';
 
-    public function generate(): void
+    private \Atgp\FacturX\Facturx $instanceFacturxLibrary;
+
+    public function __construct()
     {
-        return;
+        $this->instanceFacturxLibrary = new \Atgp\FacturX\Facturx();
+    }
+
+    /**
+     * Generate Factur-X PDF from PDF invoice and Factur-X XML.
+     *
+     * @param string $pdfInvoiceContent Content of the PDF invoice
+     * @param string $xmlInvoiceContent Content of the XML invoice
+     * @param array<int, string> $additionalAttachments Content of the additional files
+     * @param bool $addFacturxLogo Add Factur-X logo on PDF first page
+     *
+     * @return string
+     * @throws \Exception
+     */
+    public function generateFromPdfInvoiceContent(
+        string $pdfInvoiceContent,
+        string $xmlInvoiceContent,
+        array $additionalAttachments = [],
+        bool $addFacturxLogo = false
+    ): string {
+        return $this->instanceFacturxLibrary->generateFacturxFromFiles(
+            $pdfInvoiceContent,
+            $xmlInvoiceContent,
+            additionalAttachments: $additionalAttachments,
+            addFacturxLogo: $addFacturxLogo
+        );
+    }
+
+    /**
+     * Get Factur-X XML from Factur-X PDF.
+     *
+     * @param string $pdfInvoiceContent Content of the PDF invoice
+     *
+     * @return string|bool
+     * @throws \Exception
+     */
+    public function getXmlFromPdfInvoiceContent(string $pdfInvoiceContent): string|bool
+    {
+        return $this->instanceFacturxLibrary->getFacturxXmlFromPdf($pdfInvoiceContent);
+    }
+
+    /**
+     * Check Factur-X XML against XSD.
+     *
+     * @param string $xmlInvoiceContent Content of the XML invoice
+     * @param string $profile Possible values : autodetect, minimum, basicwl, basic, en16931
+     *
+     * @throws \Exception
+     *
+     * @return bool
+     */
+    public function checkXsdFromXmlInvoiceContent(string $xmlInvoiceContent, string $profile = 'autodetect'): bool
+    {
+        return $this->instanceFacturxLibrary->checkFacturxXsd($xmlInvoiceContent, $profile);
     }
 }
